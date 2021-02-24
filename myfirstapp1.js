b1e13abe2ecf13df638a891850bafe55c35be8e9
banaswaqqad <Banawaqqad7@gmail.com>
2021-02-24 16:28:33 +0200

    made some changes

--- myfirstapp1.js
+++ myfirstapp1.js
@@ -1,21 +1,5 @@
-var age = prompt("enter your age please");
-var hight = prompt("enter your hight please");
-var weight = prompt("enter your weight");
 
-if((age> 12 && hight>150 &&   50<=weight<150) || (age<12&& hight>150 &&   50<=weight<150   ) )   ;
-{
-alert("go ahead , enjoy your time");
-}
 
-elseif ((age <12 && age >65 && hight<150 && weight<50  )||(age>12 && hight<150 && weight<50) ||(age>12 && hight>150 && weight>150) )
-{
-    alert("sorry you can't play ");
-}
 
-var element1 = age;
-var element2=hight;
-var element3=weight;
-var element4 = document.getElementById('result');
-element4.innerText='this is your info '+ ' age = '+  element1 + 'hight =' + element2 + 'weight =' + element3
 
 
---
+++ mythirdapp.js
@@ -0,0 +1,6 @@
+var numofgames =0;
+var age = prompt("how many games do you want to play ");
+function getnumofgame(game){
+numofgames=parseInt(game)
+alert("your number of game is" + game );
+}
