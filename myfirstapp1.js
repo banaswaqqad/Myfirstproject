cd2c81375ecdfa1cee596435cb05d93ee62fac95
banaswaqqad <Banawaqqad7@gmail.com>
2021-02-24 16:34:31 +0200

    made some changes

--- myfirstapp1.js
+++ myfirstapp1.js
@@ -1,5 +1,16 @@
 
 
+var age = prompt("enter your age please");
+var hight = prompt("enter your hight please");
+var weight = prompt("enter your weight");
 
+if( (age> 12) && (hight>150) &&  (50<=weight<150) )   
+{
+alert("go ahead , enjoy your time");
+}
 
-
+else ((age<12) && (hight<150) && (50>weight>150))
+{
+    alert("sorry you can't play ");
+    console.log("your age is too small", + age );
+}
